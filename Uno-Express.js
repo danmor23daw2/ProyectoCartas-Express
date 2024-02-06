2855288192cc86583039c71bfc8972146ae185b1
danmor23daw2 <15586175@fje.edu>
2024-02-06 03:11:54 +0100

    Acabado Ahora Si que si que si

--- Uno-Express.js
+++ Uno-Express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 
 const partides = [];
